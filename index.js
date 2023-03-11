98de5f51e5fbf281cf1f22cca0fd9997aa950f77
Ogwenya <emanuelogwenya@outlook.com>
2023-03-11 21:16:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+let destructivelyAppendCat = (name) => {
+	cats.push(name);
+};
+
+let destructivelyPrependCat = (name) => {
+	cats.unshift(name);
+};
+
+let destructivelyRemoveLastCat = () => {
+	cats.pop();
+};
+
+let destructivelyRemoveFirstCat = () => {
+	cats.shift();
+};
+
+let appendCat = (name) => {
+	return [...cats, name];
+};
+
+let prependCat = (name) => {
+	return [name, ...cats];
+};
+
+let removeLastCat = () => {
+	return cats.slice(0, -1);
+};
+
+let removeFirstCat = () => {
+	return cats.slice(1);
+};
